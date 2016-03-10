0f92743fe71fa4a2fab9893d13a6e85fa74c022c
hzhang82 <helen.zhang@avant.com>
2016-03-09 21:28:34 -0800

    remove mocks

--- git_diff.rb
+++ git_diff.rb
@@ -4,7 +4,6 @@ require 'Logger'
 require 'json'
 require 'unirest'
 
-
 JENKINS_URL = ARGV[0]
 JOB_NAME = ARGV[1]
 BUILD_ID = ARGV[2]
@@ -18,7 +17,7 @@ def find_git_commit(build_info_json)
     next if !info.kind_of?(Hash)
     next if !info.has_key? 'lastBuiltRevision'
     next if !info.has_key? 'remoteUrls'
-    if info['remoteUrls'][0].eql? GIT_PATH
+    if info['remoteUrls'].first.eql? GIT_PATH
       return info['lastBuiltRevision']['SHA1']
     end
   end
@@ -45,22 +44,27 @@ last_known_good_sha = find_git_commit(last_known_good_info_json)
 current_build_info_json = get_build_info(BUILD_ID)
 current_sha = find_git_commit(current_build_info_json)
 
-g = Git.open(MASTER_DIRECTORY, :log => Logger.new(STDOUT))
+logger = Logger.new(STDOUT)
+g = Git.open(MASTER_DIRECTORY)
 
 #TODO: raise error when any one of the SHA doesn't exist
-raise("current changelist is not suppose to be null") if current_sha.nil?
+raise("Current changelist is not suppose to be null") if current_sha.nil? || last_known_good_sha.nil?
 puts last_known_good_sha
 puts current_sha
 
 git_diffs = g.diff(last_known_good_sha, current_sha)
 
-
-puts git_diffs
-
+offending_files = []
 for git_diff in git_diffs
   if git_diff.patch.include? MONITORING_DIRECTORY
-    exit(1)
+      logger.warn(git_diff.patch.split(/\n/).first)
+      offending_files << git_diff.patch
   end
 end
+
+if !offending_files.empty?
+  logger.error("Regions Bank Maybe affected by the differences above")
+end
+
 
 
