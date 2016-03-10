d74b0c8c89a0af9dc116a890d38cfd38b51a9af7
hzhang82 <helen.zhang@avant.com>
2016-03-09 19:02:47 -0800

    remove mocks

--- git_diff.rb
+++ git_diff.rb
@@ -10,7 +10,20 @@ JOB_NAME = ARGV[1]
 BUILD_ID = ARGV[2]
 MASTER_DIRECTORY = ARGV[3]
 #TODO: Need to allow Monitoring_directory to take array of directories
-MONITORING_DIRECTORY = ARGV[4]
+GIT_PATH = ARGV[4]
+MONITORING_DIRECTORY = ARGV[5]
+
+def find_git_commit(build_info_json)
+  for info in build_info_json['actions']
+    next if !info.kind_of?(Hash)
+    next if !info.has_key? 'lastBuiltRevision'
+    next if !info.has_key? 'remoteUrls'
+    if info['remoteUrls'][0].eql? GIT_PATH
+      return info['lastBuiltRevision']['SHA1']
+    end
+  end
+  nil
+end
 
 jenkins_generic_info_json_url = JENKINS_URL + 'job/' + JOB_NAME + '/api/json?'
 jenkins_generic_info_json = JSON.parse(Unirest.get(jenkins_generic_info_json_url).raw_body)
@@ -19,12 +32,12 @@ x = JSON.parse(Unirest.get(last_known_good_build_url).raw_body)
 last_known_good_sha = JSON.parse(Unirest.get(last_known_good_build_url).raw_body)['actions'][3]['lastBuiltRevision']['SHA1']
 
 current_build_url = JENKINS_URL + 'job/' + JOB_NAME + '/' + BUILD_ID + '/api/json?'
-
-current_sha = JSON.parse(Unirest.get(current_build_url).raw_body)['actions'][3]['lastBuiltRevision']['SHA1']
+current_build_info_json = JSON.parse(Unirest.get(current_build_url).raw_body)
+current_sha = find_git_commit(current_build_info_json)
 
 g = Git.open(MASTER_DIRECTORY, :log => Logger.new(STDOUT))
 
-
+#TODO: raise error when any one of the SHA doesn't exist
 puts "last_known_good_sha " + last_known_good_sha
 puts "current_sha " + current_sha
 
@@ -41,3 +54,5 @@ for git_diff in git_diffs
     exit(1)
   end
 end
+
+
