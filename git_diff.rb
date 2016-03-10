21915f5f4f5cc473b7c1babcddf9ef3739c989bd
Helen Zhang <hzhang82@mbp-0630.ad.avant.com>
2016-03-09 16:17:17 -0800

    test

--- git_diff.rb
+++ git_diff.rb
@@ -4,21 +4,24 @@ require 'Logger'
 require 'json'
 require 'unirest'
 
+
 JENKINS_URL = ARGV[0]
 JOB_NAME = ARGV[1]
 BUILD_ID = ARGV[2]
 MASTER_DIRECTORY = ARGV[3]
-#Need to allow Monitoring_directory to take array of directories
+#TODO: Need to allow Monitoring_directory to take array of directories
 MONITORING_DIRECTORY = ARGV[4]
 
+=begin
 #mocks
-#JOB_NAME = 'job/Test/'
-#JENKINS_URL = "http://localhost:8080/"
-#BUILD_ID = 19
-#MASTER_DIRECTORY = '/Users/hzhang82/repo/avant-cli'
-#MONITORING_DIRECTORY = ARGV[4]
+JOB_NAME = 'job/Test/'
+JENKINS_URL = "http://localhost:8080/"
+BUILD_ID = 19
+MASTER_DIRECTORY = '/Users/hzhang82/repo/avant-cli'
+MONITORING_DIRECTORY = ARGV[4]
+=end
 
-jenkins_generic_info_json_url = JENKINS_URL + JOB_NAME + '/api/json?'
+jenkins_generic_info_json_url = JENKINS_URL + 'job/' + JOB_NAME + '/api/json?'
 jenkins_generic_info_json = JSON.parse(Unirest.get(jenkins_generic_info_json_url).raw_body)
 last_known_good_build_url = jenkins_generic_info_json['lastSuccessfulBuild']['url'] + '/api/json?'
 last_known_good_sha = JSON.parse(Unirest.get(last_known_good_build_url).raw_body)['actions'][1]['lastBuiltRevision']['SHA1']
